72b600c07daa0a67d06e4072e34337e068729b72
Boldizsarb <5boldb36@solent.ac.uk>
2022-03-29 17:25:38 +0100

    first_sql_code

--- sql_and_python/testing.py
+++ sql_and_python/testing.py
@@ -1,1 +1,16 @@
-print("somehting")
+import sqlite3
+
+db_file = "/Users/boldizsarbanfia/Documents/University/Database/Databases/testeing.db"
+db = sqlite3.connect(db_file)
+cursor = db.cursor()
+ # Now comest he query
+
+sql_query = "SELECT empno, ename, deptno FROM emp"
+cursor.execute(sql_query)    # executing the query
+#emp_row = cursor.fetchone() # retrievign the first row
+all_emp_rows = cursor.fetchall() # retrieving all rows
+for emp_row in all_emp_rows:
+    empno = emp_row[0]
+    ename = emp_row[1]
+    deptno = emp_row[2]
+    print("{0},{1},{2}".format(empno, ename, deptno))
