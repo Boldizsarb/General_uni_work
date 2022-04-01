c4b3b03675a332d38ad5e8ff263812f785705c8b
Boldizsarb <5boldb36@solent.ac.uk>
2022-04-01 16:02:38 +0100

    node

--- Webtech/Nodejs/index.js
+++ Webtech/Nodejs/index.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const express = require('express'); 
+
+const app = express();
+
+app.get("/", (req, res) =>{
+res.send("<h1>Welcome ot the gome page</h1>");
+});
+// creating another root:
+app.get("/contact", (req, res) =>{
+    res.send("<h1>Contact page</h1>");
+    });
+
+app.listen(3000,()=>{console.log("Server started!")} )
