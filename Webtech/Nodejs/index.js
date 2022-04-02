5111285b9a1c9731896d5ede762639370d2d1d22
Boldizsarb <5boldb36@solent.ac.uk>
2022-04-02 11:29:45 +0100

    node_first_part

--- Webtech/Nodejs/index.js
+++ Webtech/Nodejs/index.js
@@ -5,10 +5,13 @@ app.set('view engine', 'ejs'); // EJS, HTML only
 app.use(express.static('public')); // CSS and Java 
 
 app.get("/", (req, res) =>{
-res.render("index"); // file name only. it will rendered onto the page 
+res.render("index",{title: "Welcome to the Home page"}); // file name only. it will rendered onto the page 
 });
 app.get("/contact", (req, res) =>{
     res.render("contact"); 
     });
+    app.get("/aboutus", (req, res) =>{
+        res.render("aboutus"); 
+        });
 
 app.listen(3000,()=>{console.log("Server started!")} );
