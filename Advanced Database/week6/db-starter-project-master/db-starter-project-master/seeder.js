9176d7001c81abd62e46a5979487293cf4be2ea7
Boldizsarb <5boldb36@solent.ac.uk>
2022-11-17 12:13:39 +0000

    seeder

--- Advanced Database/week6/db-starter-project-master/db-starter-project-master/seeder.js
+++ Advanced Database/week6/db-starter-project-master/db-starter-project-master/seeder.js
@@ -1,3 +1,4 @@
+
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const fs = require("fs").promises;
@@ -11,6 +12,8 @@ const { MONGODB_URI } = process.env;
  */
 const client = new MongoClient(MONGODB_URI);
 
+
+
 async function main() {
   try {
     await client.connect();
@@ -21,9 +24,7 @@ async function main() {
      * If existing records then delete the current collections
      */
     if (results) {
-      console.info("deleting collection");
-      await db.collection("tastings").drop();
-      await db.collection("tasters").drop();
+      db.dropDatabase();
     }
 
     /**
@@ -69,40 +70,88 @@ async function main() {
     const wineTasters = await wineTastersRef.toArray();
     await db.collection("tasters").insertMany(wineTasters);
 
-    /** Our final data manipulation is to reference each document in the
-     * tastings collection to a taster id
+    /** This data manipulation is to reference each document in the
+     * tastings collection to a taster id. Further to this we also take the opportunity to
+     * tidy up points (converting it to a int) and regions, adding them to a an array
      */
 
     const updatedWineTastersRef = db.collection("tasters").find({});
     const updatedWineTasters = await updatedWineTastersRef.toArray();
     updatedWineTasters.forEach(async ({ _id, name }) => {
-      await db
-        .collection("tastings")
-        .updateMany({ taster_name: name }, [
-          { $set: { taster_id: _id, regions: ["$region_1", "$region_2"] } },
-        ]);
-      /**
-       * we can get rid of region_1/2 off our root document, since we've
-       * placed them in an array
-       */
-      await db
-        .collection("tastings")
-        .updateMany({}, { $unset: { region_1: "", region_2: " " } });
-
-      /**
-       * Finally, we remove nulls regions from our collection of arrays
-       *  */
-      await db
-        .collection("tastings")
-        .updateMany({ regions: { $all: [null] } }, [
-          { $set: { regions: [{ $arrayElemAt: ["$regions", 0] }] } },
-        ]);
-      load.stop();
-      console.info(
-        `Wine collection set up! 🍷🍷🍷🍷🍷🍷🍷 \n I've also created a tasters collection for you 🥴 🥴 🥴`
-      );
-      process.exit();
+      await db.collection("tastings").updateMany({ taster_name: name }, [
+        {
+          $set: {
+            taster_id: _id,
+            regions: ["$region_1", "$region_2"],
+            points: { $toInt: "$points" },
+          },
+        },
+      ]);
     });
+
+
+    /**
+     * we can get rid of region_1/2 off our root document, since we've
+     * placed them in an array
+     */
+    await db
+      .collection("tastings")
+      .updateMany({}, { $unset: { region_1: "", region_2: " " } });
+
+    /**
+     * Finally, we remove nulls regions from our collection of arrays
+     * */
+    await db
+      .collection("tastings")
+      .updateMany({ regions: { $all: [null] } }, [
+        { $set: { regions: [{ $arrayElemAt: ["$regions", 0] }] } },
+      ])
+
+
+    db.collection("tastings").aggregate([
+      { $group: { _id: "$variety" } },
+      { $project: { name: "$_id", "_id": 0 } },
+      { $out: "varieties" }
+    ]).toArray();
+
+    db.collection("tastings").aggregate([
+      { $group: { _id: "$country" } },
+      { $project: { name: "$_id", "_id": 0 } },
+      { $out: "countries" }
+    ]).toArray()
+
+
+
+    await db.collection("tastings").aggregate([
+      { $group: { _id: "$province" } },
+      { $project: { name: "$_id", "_id": 0 } },
+      { $out: "provinces" }
+    ]).toArray()
+
+    await db.collection("tastings").aggregate([
+      { $unwind: "$regions" },
+      { $group: { _id: "$regions" } },
+      { $project: { name: '$_id', _id: 0 } },
+      { $out: "regions" }
+    ]).toArray();
+
+
+    await db.collection("tastings").aggregate([
+      { $unwind: "$regions" },
+      { $group: { _id: "$regions" } },
+      { $project: { name: "$_id", "_id": 0 } },
+      { $out: "regions" }
+    ]).toArray() 
+
+
+
+    load.stop();
+    console.info(
+      `Wine collection set up! 🍷🍷🍷🍷🍷🍷🍷 \n I've also created a tasters collection for you 🥴 🥴 🥴`
+    );
+
+
+    process.exit();
   } catch (error) {
     console.error("error:", error);
     process.exit();
